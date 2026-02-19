f287a4a9201df112bac0a0b949f69e1df2a4124b
P-KAVYASRI <kavyasri369369@gmail.com>
2026-02-19 12:22:13 +0530

    update9

--- client/src/sections/FeaturesSection.tsx
+++ client/src/sections/FeaturesSection.tsx
@@ -8,7 +8,7 @@ import type { IFeature } from "../types";
 export default function FeaturesSection() {
     return (
         <div id="features" className="px-4 md:px-16 lg:px-24 xl:px-32">
-            <SectionTitle text1="Features" text2="What you get" text3="Components, patterns and pages — everything you need to ship." />
+            <SectionTitle text1="Features" text2="Why use our generator?" text3="Create stunning thumbnails that get clicks, without the hassle." />
             <div className="flex flex-wrap items-center justify-center gap-6 md:gap-4 mt-16 px-6">
                 {featuresData.map((feature: IFeature, index: number) => (
                     <motion.div key={index} className={`${index === 1 ? 'p-px rounded-[13px] bg-linear-to-br from-pink-600 to-slate-800' : ''}`}
@@ -18,7 +18,7 @@ export default function FeaturesSection() {
                         transition={{ delay: index * 0.15, type: "spring", stiffness: 320, damping: 70, mass: 1 }}
                     >
                         <div className="p-6 rounded-xl space-y-4 border border-slate-800 bg-slate-950 max-w-80 w-full">
-                            {feature.icon}
+                           <img src= {feature.icon} alt={feature.title}/>
                             <h3 className="text-base font-medium text-white">
                                 {feature.title}
                             </h3>
@@ -37,7 +37,7 @@ export default function FeaturesSection() {
                     viewport={{ once: true }}
                     transition={{ type: "spring", stiffness: 320, damping: 70, mass: 1 }}
                 >
-                    PrebuiltUI helps you build faster by transforming your design vision into fully functional, production-ready UI components.
+                    Our AI-powered thumbnail generator is designed to help you create eye-catching thumbnails that drive clicks and engagement. With our smart analysis, you can easily create thumbnails that resonate with your audience, increasing your click-through rates and boosting your content's visibility. Whether you're a content creator, marketer, or business owner, our tool is here to help you stand out in the crowded digital landscape.
                 </motion.p>
                 <div className="grid grid-cols-1 md:grid-cols-3 mt-8 gap-10">
                     <motion.div className="md:col-span-2"
@@ -66,4 +66,4 @@ export default function FeaturesSection() {
             </div>
         </div>
     );
-}
+}  //32.41
